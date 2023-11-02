89b2b157c557acb5c1df486a4127f8e99b7605e9
Makinde Oluwasegun <bryanconquer@gmail.com>
2023-11-02 13:07:26 +0100

    smart contract

--- contracts/marketplace.sol
+++ contracts/marketplace.sol
@@ -1,40 +1,39 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
-import "hardhat/console.sol"
-import "@openzeppelin/contract/utils/ERC721";
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract nftMarketPlace is ERC721URIStorage {
 
-    using Counters for Counters.counter;
+    using Counters for Counters.Counter;
 
-    Counters.counter private _tokenIds;
+    Counters.Counter private _tokenIds;
 
-    Counters.counter private _itemSold;
+    Counters.Counter private _itemSold;
 
     address private owner;
 
     uint256 listingPrice = 0.1 ether;
 
     struct nftData {
-        uint256 _tokenId,
-        address payable _owner,
-        address payable _seller,
-        uint256 _nftprice,
-        bool _nftListed
+        uint256 _tokenId;
+        address payable _owner;
+        address payable _seller;
+        uint256 _nftprice;
+        bool _nftListed;
     }
 
-    event nftListed {
+    event nftListed (
         uint256 indexed _tokenId,
-        address payable _owner,
-        address payable _seller,
+        address payable owner,
+        address payable seller,
         uint256 _nftPrice,
         bool _nftListed
-    }
-
+    );
 
     mapping(uint256 => nftData) private _idTonftData;
 
@@ -60,19 +59,20 @@ contract nftMarketPlace is ERC721URIStorage {
         return _idTonftData[tokenId];
     }
 
-    function getCurrentToken() public view returns (nftData memory) {
+    function getCurrentToken() public view returns (uint256) {
         return _tokenIds.current();
     }
 
-    function listNft (string memory _nftURI, uint256 _nftPrice) public payable returns (uint256) {
+    function listNft (string memory _nftURI, uint256 _nftPrice) public payable returns (uint) {
         _tokenIds.increment();
 
         uint256 newNft =  _tokenIds.current();
-        require(msg.sender != address(0), "Invalid address")
+        require(msg.sender != address(0), "Invalid address");
         _safeMint(msg.sender, newNft);
 
         _setTokenURI(newNft, _nftURI);
         
+        createListedTokens(newNft, _nftPrice);
 
         return newNft;
     }
@@ -82,36 +82,36 @@ contract nftMarketPlace is ERC721URIStorage {
 
         require (_nftPrice > 0, "invalid price amount");
 
-        _idTonftData[tokenId] = nftData {
-        uint256 tokenId,
+        _idTonftData[tokenId] = nftData (
+        tokenId,
         payable (address(this)),
         payable (msg.sender),
-        uint256 _nftPrice,
+        _nftPrice,
         true
-        }
+        );
 
-        _transfer(msg.sender, address(this), tokenId)
+        _transfer(msg.sender, payable(address(this)), tokenId);
 
-        emit TokenlistedSuccess {
+        emit nftListed (
         tokenId,
-        address(this),
-        msg.sender,
-        uint256 _nftPrice,
-        bool true
-        }
+        payable(address(this)),
+        payable(address(msg.sender)),
+        _nftPrice,
+        true
+        );
     }
 
     function GetListedNft () public view returns (nftData[] memory) {
-        uint256 listedNft = _tokenId.current();
-        nftData[] memory tokens = new nftData[](listedNft)
+        uint256 listedNft = _tokenIds.current();
+        nftData[] memory tokens = new nftData[](listedNft);
 
         uint currentIndex = 0;
         uint currentId;
 
         for(uint i = 0; i < listedNft; i++) {
             currentId = i + 1;
-            nftData[] storage currentItems =  _idTonftData[currentId]
-            token[currentIndex] = currenItems;
+            nftData storage currentItems =  _idTonftData[currentId];
+            tokens[currentIndex] = currentItems;
             currentIndex += 1;
         }
 
@@ -121,23 +121,23 @@ contract nftMarketPlace is ERC721URIStorage {
     // Returns all the NFTs that the current user is owner or seller in
 
     function UserNft () public view returns (nftData[] memory) {
-        uint userNft = _tokenId.current();
-        nftData[] memory listUserNft = new nftData[](userNft)
+        uint userNft = _tokenIds.current();
+        nftData[] memory listUserNft = new nftData[](userNft);
 
         uint itemCount = 0;
         uint userNftIndex = 0;
         uint userNftCurrentId;
 
         for (uint i = 0; i < userNft; i++) {
-            if (_idTonftData[i+1].owner == msg.sender || _idTonftData[i+1].seller == msg.sender ) 
+            if (_idTonftData[i+1]._owner == msg.sender || _idTonftData[i+1]._seller == msg.sender ) 
             itemCount += 1;
         }
 
         for (uint i = 0; i < userNft; i++) {
-            if (_idTonftData[i+1].owner == msg.sender || _idTonftData[i+1].seller == msg.sender){
+            if (_idTonftData[i+1]._owner == msg.sender || _idTonftData[i+1]._seller == msg.sender){
                 userNftCurrentId ++;
                 
-                nftData[] storage nftItems = _idTonftData[userNftCurrentId]
+                nftData storage nftItems = _idTonftData[userNftCurrentId];
                 listUserNft[userNftIndex] = nftItems;
                 userNftCurrentId ++;
             }
@@ -146,29 +146,23 @@ contract nftMarketPlace is ERC721URIStorage {
     }
 
     function executeSale (uint256 _tokenId) public payable {
-        uint price =  _idTonftData[_tokenId].price;
-        address seller =  _idTonftData[_tokenId].seller;
+        uint price =  _idTonftData[_tokenId]._nftprice;
+        address seller =  _idTonftData[_tokenId]._seller;
 
         require (msg.value == price, "insurficient token amount" );
 
         _idTonftData[_tokenId]._nftListed =  true;
-        _idTonftData[_tokenId].seller =  msg.seller;
+        _idTonftData[_tokenId]._seller =  payable (msg.sender);
 
-        _itemSold.increment()
+        _itemSold.increment();
 
-        _transfer(address(this, msg.sender, _tokenId));
+        _transfer(address(this), msg.sender, _tokenId);
 
         approve(address(this), _tokenId);
 
-        payable(owner)._transfer(listingPrice);
+        payable(owner).transfer(listingPrice);
 
-        payable(seller)._transfer(msg.value);
+        payable(seller).transfer(msg.value);
 
-    }
-
-    function deListNft (uint256 _tokenId) public returns (bool) {
-        require (_owner == address(this), "only owner can call this");
-        _burn(_tokeId);
-        return true;
     }
 }
